4dc6b514fcc4d1e5ebef6f18af38494233acabf9
nikawadori <nikawa.2161.t@icloud.com>
2020-10-28 22:37:21 +0900

    Clean

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_article, only: [:edit, :update]
+  
   def index
     @boards = Board.all
   end
@@ -22,11 +24,29 @@ class BoardsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @board .update(board_params)
+      redirect_to board_path(@board), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
     @board = current_user.boards.find(params[:id])
+    @board.destroy!
+    redirect_to root_path, notice: '削除しました'
   end
 
   private
   def board_params
     params.require(:board).permit(:title, :content)
+  end
+
+  def set_article
+    @board = current_user.boards.find(params[:id])
   end
 end
